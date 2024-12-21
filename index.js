926baef7f1fbb331b09bcce75b5d2df5b0bfbde6
“sumit” <sumit.ducat@gmail.com>
2024-12-21 14:47:20 +0530

    product commit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app=express();
 app.get("/",(req,res)=>{
     res.json({"err":0,"msg":"My Application"})
 })
+app.get("/products",(req,res)=>{
+    res.json({"err":0,"msg":"My products"})
+})
 app.listen(PORT,(err)=>{
     console.log(`Server work on ${PORT}`)
 })
